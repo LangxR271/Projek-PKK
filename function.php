9d526b1551c0a645cccef12358cba118e51fc10a
LangxR271 <114453890+LangxR271@users.noreply.github.com>
2022-09-27 12:27:33 +0700

    Add files via upload

---
+++ function.php
@@ -0,0 +1,42 @@
+<?php
+$data=[
+    [
+        "identitas" => "Rizkan<br>
+        17-03-2006<br>
+        Benda<br>",
+    "foto" => "Kan.jpg"
+    ],
+    [
+        "identitas" => "Kelana<br>
+        04-09-2005<br>
+        Kalangsari<br>",
+    "foto" => "Bang.jpg"
+    ],
+    [
+        "identitas" => "Gilang<br>
+        12-10-2005<br>
+        Babakan New<br>",
+    "foto" => "Lang.jpg"
+    ]
+    ];
+    $produk=[
+    [
+        "nama_p" => "chesse cake",
+     "harga" => "5K",
+    "stock" => "25",
+    "foto" => "Chesse.jpg"
+    ],
+    [
+        "nama_p" => "Risol Mayo",
+     "harga" => "2K",
+    "stock" => "20",
+    "foto" => "risol.jpeg"
+    ],
+    [
+        "nama_p" => "Agar Agar",
+     "harga" => "2,5K",
+    "stock" => "15",
+    "foto" => "agar.jpeg"
+    ]
+];
+?>
